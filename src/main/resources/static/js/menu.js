3b0fede55ed10b98fc974f2777799e84fc7c06a1
Hobook <37508173+HOBOOK@users.noreply.github.com>
2020-01-02 01:19:53 +0900

    스크롤바 커스터마이징, 스크롤 이동 추가

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -8,3 +8,33 @@ burger.each(function(index){
         $(this).toggleClass('active');
     })
 });
+
+
+$(".menu_click").on("click", function (e) {
+    e.preventDefault();//anchor이벤트의 기본동작을 막는다.
+    var thisTarget = $(this).attr("href");
+    var moveTarget = $(".mCSB_container");
+    var topPos = $("#"+thisTarget).position().top;
+    if(-moveTarget.position().top<topPos)
+    {
+        moveTarget.animate({top:-topPos-30},500);
+    }
+    else
+    {
+        moveTarget.animate({top:-topPos+30},500);
+    }
+    moveTarget.animate({top:-topPos},500);
+
+});
+
+
+$(window).on('load',(function() {
+    $('.container').mCustomScrollbar();
+}));
+
+$('.container').mCustomScrollbar({
+
+    theme: "minimal-dark",
+    axis:"y" // horizontal scrollbar
+});
+

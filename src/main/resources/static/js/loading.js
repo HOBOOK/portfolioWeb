9ba1ea62b3c7d838145e7f6eb1f1d96a0710932f
Hobook <37508173+HOBOOK@users.noreply.github.com>
2020-01-06 00:24:49 +0900

    모바일 스크롤 수정

--- src/main/resources/static/js/loading.js
+++ src/main/resources/static/js/loading.js
@@ -6,7 +6,12 @@ $(window).on('load',(function() {
     },1000);
 }));
 
-
+window.addEventListener('load', function() {
+    // body의 height를 살짝 늘리는 코드
+    document.body.style.height = (document.documentElement.clientHeight + 5) + 'px';
+    // scroll를 제어 하는 코드
+    setTimeout(scrollTo, 0, 0, 1);
+}, false);
 
 
 
